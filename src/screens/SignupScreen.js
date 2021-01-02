b5824ff4fe9eb294f5a28231587fe2532e76748b
rajabovjavid <jvdrjbv@gmail.com>
2021-01-02 23:05:17 +0400

    navigations are configured

---
+++ src/screens/SignupScreen.js
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+import { Text, StyleSheet, View, Button } from "react-native";
+
+const SampleScreen = () => {
+    return(
+        <View>
+            <Text>Sample Screen</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+
+})
+
+export default SampleScreen
